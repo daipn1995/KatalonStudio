99308244f7cfba1937adfef9fea26c25c0e77116
Dai Pham Ngoc <daipham.ngoc.ext@tba.group>
2022-04-07 10:53:25 +0700

    Them quyen sach toan lop 1 mau hong

---
+++ Test Suites/Login.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>2ee4e52d-678d-4f99-b627-7beeba20d9ce</testSuiteGuid>
+</TestSuiteEntity>
